97be84ea6350f1ded41c1181119c0bd8da6f6f4f
ikeikeikeike <jp.ne.co.jp@gmail.com>
2021-01-01 05:52:40 +0900

    can be used, iterator, generator, list

--- src/free.rs
+++ src/free.rs
@@ -3,15 +3,26 @@ use pyo3::prelude::*;
 use pyo3::types::{PyAny, PyList};
 
 #[pyfunction]
-pub fn all(_py: Python, iterable: &PyList, f: &PyAny) -> PyResult<bool> {
+pub fn all(_py: Python, iterable: &PyAny, f: &PyAny) -> PyResult<bool> {
     if !f.is_callable() {
         return Err(PyTypeError::new_err("predicate is not callable"));
     }
+    let iter = iterable.iter();
+    if iter.is_err() {
+        return Err(iter.unwrap_err());
+    }
 
-    Ok(itertools::all(iterable.iter(), |elt| {
-        match f.call1((elt,)).map(|r| r.is_true().unwrap_or(false)) {
+    let r = itertools::all(iter.unwrap(), |elt| {
+        if elt.is_err() {
+            return false; // XXX: raise error
+        }
+
+        let ok = f.call1((elt.unwrap(),));
+        match ok.map(|r| r.is_true().unwrap_or(false)) {
             Ok(r) => r,
             _ => false,
         }
-    }))
+    });
+
+    Ok(r)
 }
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,14 +1,13 @@
 import operator
 
+import pytest
+
 from and_itertools import all as itertools_all
 
 
-# def test_generator():
-#     assert itertools_all(range(1, 3), operator.truth) is True
-
-
-def test_builtin():
-    assert itertools_all([1, 2, 3], operator.truth) is True
+def test_not_iterable():
+    with pytest.raises(TypeError):
+        itertools_all(None, operator.truth)
 
 
 def test_ok():
@@ -16,8 +15,36 @@ def test_ok():
 
 
 def test_ng():
+    assert itertools_all([0, 1, 2, False], operator.truth) is False
+
+
+def test_iterator_ok():
+    assert itertools_all(iter([1, 2, 3, 4]), operator.truth) is True
+
+
+def test_iterator_ng():
+    assert itertools_all(iter([0, 1, 2, 3, 4]), operator.truth) is False
+
+
+def test_generator_ok():
+    assert itertools_all(range(1, 3), operator.truth) is True
+
+
+def test_generator_ng():
+    assert itertools_all(range(0, 3), operator.truth) is False
+
+
+def test_generator_types():
+    assert itertools_all((i for i in [1, "2", False, None]), lambda x: isinstance(x, (str,))) is False
+
+
+def test_builtin():
+    assert itertools_all([1, 2, 3], operator.truth) is True
+
+
+def test_predicate():
     assert itertools_all([1, 2, 3], lambda x: isinstance(x, (str, bool))) is False
 
 
-def test_any():
+def test_types():
     assert itertools_all([1, "2", False, None], lambda x: isinstance(x, (str,))) is False
