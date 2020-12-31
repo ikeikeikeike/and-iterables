37a434ac76575cc6f62e0da94092689ad10b6551
ikeikeikeike <jp.ne.co.jp@gmail.com>
2021-01-01 06:18:14 +0900

    error handle

--- src/free.rs
+++ src/free.rs
@@ -1,25 +1,20 @@
 use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
-use pyo3::types::{PyAny, PyList};
+use pyo3::types::PyAny;
 
 #[pyfunction]
 pub fn all(_py: Python, iterable: &PyAny, f: &PyAny) -> PyResult<bool> {
     if !f.is_callable() {
         return Err(PyTypeError::new_err("predicate is not callable"));
     }
-    let iter = iterable.iter();
-    if iter.is_err() {
-        return Err(iter.unwrap_err());
-    }
 
-    let r = itertools::all(iter.unwrap(), |elt| {
+    let r = itertools::all(iterable.iter()?, |elt| {
         if elt.is_err() {
-            return false; // XXX: raise error
+            return false; // XXX: Return Err?
         }
 
-        let ok = f.call1((elt.unwrap(),));
-        match ok.map(|r| r.is_true().unwrap_or(false)) {
-            Ok(r) => r,
+        match f.call1((elt.unwrap(),)).and_then(|r| r.is_true()) {
+            Ok(ok) => ok,
             _ => false,
         }
     });
